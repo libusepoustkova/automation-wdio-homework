66030ac3d6d8b7031b8744f3cfe0e2eb85273c61
libuse.poustkova <libuse.poustkova@skodagroup.com>
2022-12-21 12:44:14 +0100

    first commit

--- specs/order.e2e.js
+++ specs/order.e2e.js
@@ -9,9 +9,85 @@ import {
     startDate,
     endDate
 } from './fixtures.js'
+import {getFieldValueById} from "../pages/functions";
 
-describe('Objednávka pro MŠ/ZŠ', () => {
+describe ('Navigace_do_objednavky', () => {
+    beforeEach(() => {
+        browser.reloadSession();
+        browser.url('');
+    });
+    it('should show form for order', () => {
+        const pro_ucitele = $('.dropdown-toggle=Pro učitelé');
+        // console.log(pro_ucitele.getText(), pro_ucitele.getTagName());
+        const order_forMSZS = $('.dropdown-item=Objednávka pro MŠ/ZŠ');
+        pro_ucitele.click();
+        expect(order_forMSZS).toBeDisplayed();
+        order_forMSZS.click()
+        const header = $('header').$('h1*=Nová objednávka')
+        expect(header).toBeDisplayed();
+    });
+});
 
-    // testy sem :)
 
+describe('Objednavka_pro_MSZS', () => {
+    beforeEach(() => {
+        browser.reloadSession();
+        browser.url('objednavka/pridat');
+    });
+
+     it('should auto fill items when set ICO ', () => {
+        const header = $('header').$('h1*=Nová objednávka')
+        const ico = $('#ico');
+        ico.setValue(ICO);
+        const client = $('#client');
+        browser.keys('Enter');
+        // browser.pause(5000);
+        const toastMessage = $('.toast-message');
+        toastMessage.waitForExist();
+        expect(toastMessage.getText()).toEqual('Data z ARESu úspěšně načtena');
+        const fieldName = getFieldValueById('client');
+        const fieldAddress = getFieldValueById('address');
+        // console.log(fieldName, fieldAddress)
+        expect(fieldName).toEqual(clientName);
+        expect(fieldAddress).toEqual(address);
+        browser.saveScreenshot('vyplneno.png');
+
+    });
+    it('should not auto fill items when set invalid ICO ', () => {
+        const ico = $('#ico');
+        ico.setValue('12345');
+        const client = $('#client');
+        browser.keys('Enter');
+        const toastMessage = $('.toast-message');
+        toastMessage.waitForExist();
+        expect(toastMessage.getText()).toEqual('IČO nenalezeno, zkontrolujte jej prosím');
+        const fieldName = getFieldValueById('client');
+        const fieldAddress = getFieldValueById('address');
+        expect(fieldName).toEqual('');
+        expect(fieldAddress).toEqual('');
+        browser.saveScreenshot('vyplneno2.png');
+    });
+
+     it('should create order', () => {
+        browser.url('objednavka/pridat');
+        const header = $('header').$('h1*=Nová objednávka')
+        expect(header).toBeDisplayed();
+        const ico = $('#ico');
+        ico.setValue(ICO);
+        const client = $('#client');
+        browser.keys('Enter');
+        // browser.pause(5000);
+        const toastMessage = $('.toast-message');
+        toastMessage.waitForExist();
+        // na stránce je toast message
+        expect(toastMessage.getText()).toEqual('Data z ARESu úspěšně načtena');
+        browser.saveScreenshot('vyplneno.png');
+
+        const fieldName = getFieldValueById('client');
+        const fieldAddress = getFieldValueById('address');
+
+        console.log(fieldName, fieldAddress)
+        expect(fieldName).toEqual(clientName);
+        expect(fieldAddress).toEqual(address);
+    });
 });
