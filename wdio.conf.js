2f824c32f8f342a1ccd7188bb61ff009b50e0ef9
libuse.poustkova <libuse.poustkova@skodagroup.com>
2023-01-02 13:18:19 +0100

    added files for configuration

---
+++ wdio.conf.js
@@ -0,0 +1,45 @@
+exports.config = {
+    specs: [
+        './specs/**/*.js'
+    ],
+    exclude: [
+        './specs/examples/**/*.js'
+    ],
+    maxInstances: 10,
+    capabilities: [{
+        maxInstances: 5,
+        browserName: 'opera',
+        acceptInsecureCerts: true,
+        'goog:chromeOptions': {
+            args: [
+                // '--window-size=1920,1080',
+                //'--headless',
+                '--no-sandbox',
+                '--disable-gpu',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-infobars'
+            ]
+        },
+        "moz:firefoxOptions": {
+            args: [
+                // '-headless'
+            ]
+        }
+    }],
+    logLevel: 'silent', // trace | debug | info | warn | error | silent
+    bail: 0,
+    baseUrl: 'https://team8-2022brno.herokuapp.com',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    services: [
+        'selenium-standalone'
+    ],
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    }
+}
